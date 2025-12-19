6873cb9207d99de4f6ffaefed4d0c6b77a33ca5c
hanze06@163.com <hanze06@163.com>
2025-12-20 01:36:55 +0800

    update sorting

---
+++ src/Sorting Algorithms/sortingAlgorithms.cpp
@@ -0,0 +1,29 @@
+#include "sortingAlgorithms.hpp"
+
+std::expected<void, DataStructureError> bubbleSort(std::vector<T>& vec, Comparator comp) {
+    
+}
+
+std::expected<void, DataStructureError> selectionSort(std::vector<T>& vec, Comparator comp) {
+    
+}
+
+std::expected<void, DataStructureError> insertionSort(std::vector<T>& vec, Comparator comp) {
+    
+}
+
+std::expected<void, DataStructureError> shellSort(std::vector<T>& vec, Comparator comp) {
+    
+}
+
+std::expected<void, DataStructureError> mergeSort(std::vector<T>& vec, Comparator comp) {
+    
+}
+
+std::expected<void, DataStructureError> quickSort(std::vector<T>& vec, Comparator comp) {
+    
+}
+
+std::expected<void, DataStructureError> heapSort(std::vector<T>& vec, Comparator comp) {
+    
+}
--- src/Sorting Algorithms/sortingAlgorithms.hpp
+++ src/Sorting Algorithms/sortingAlgorithms.hpp
@@ -5,25 +5,37 @@
 #include "../Error.hpp"
 
 template<typename KeyExtractor>
-static auto compare(KeyExtractor extractor);
+static auto compareAsc(KeyExtractor extractor) {
+    return [extractor](const T& a, const T& b) {
+        return extractor(a) < extractor(b);
+    };
+}
+
+template<typename KeyExtractor>
+static auto compareDesc(KeyExtractor extractor) {
+    return [extractor](const T& a, const T& b) {
+        return extractor(a) > extractor(b);
+    };
+}
+
+template<typename T, typename Comparator>
+static std::expected<void, DataStructureError> bubbleSort(std::vector<T>& vec, Comparator comp);
 
 template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> bubbleSort(std::vector<T>& vec, Comparator comp = Comparator());
+static std::expected<void, DataStructureError> selectionSort(std::vector<T>& vec, Comparator comp);
 
 template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> selectionSort(std::vector<T>& vec, Comparator comp = Comparator());
+static std::expected<void, DataStructureError> insertionSort(std::vector<T>& vec, Comparator comp);
 
 template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> insertionSort(std::vector<T>& vec, Comparator comp = Comparator());
+static std::expected<void, DataStructureError> shellSort(std::vector<T>& vec, Comparator comp);
 
 template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> shellSort(std::vector<T>& vec, Comparator comp = Comparator());
+static std::expected<void, DataStructureError> mergeSort(std::vector<T>& vec, Comparator comp);
 
 template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> mergeSort(std::vector<T>& vec, Comparator comp = Comparator());
+static std::expected<void, DataStructureError> quickSort(std::vector<T>& vec, Comparator comp);
 
 template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> quickSort(std::vector<T>& vec, Comparator comp = Comparator());
+static std::expected<void, DataStructureError> heapSort(std::vector<T>& vec, Comparator comp);
 
-template<typename T, typename Comparator = std::less<T>>
-static std::expected<void, DataStructureError> heapSort(std::vector<T>& vec, Comparator comp = Comparator());
